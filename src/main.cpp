5951ab29a1610326371de4fcee0f03f898367508
Syed Abbad Hasan Bokhari <abbadhasan750@gmail.com>
2025-04-17 10:10:58 +0500

    restarting using harward course

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "raylib.h"
-#include "Bird.h"
 
 int main() {
     const int screenWidth = 1200;
@@ -8,17 +7,13 @@ int main() {
     SetTargetFPS(60);
 
     float groundY = 700;  // Adjust ground position for bigger screen
-    Bird bird({200, groundY - 30}, 20.0f, groundY, screenWidth, screenHeight);
 
     while (!WindowShouldClose()) {
-        bird.HandleInput();
-        bird.Update();
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawLine(0, groundY, screenWidth, groundY, DARKGRAY); // Draw ground
-        bird.Draw();
 
         DrawText("Drag and release to launch!", 20, 20, 24, DARKGRAY);
 
